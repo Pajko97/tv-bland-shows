29fb08effbe7e0bbaa7fa42dc04172e8861b4778
pavle <pavle@git.topshipment.com>
2022-02-15 10:35:06 +0100

    Code cleanup, added translation

--- src/api/shows-api.ts
+++ src/api/shows-api.ts
@@ -1,19 +1,18 @@
 import Schedule from "../models/Schedule";
 import Show from "../models/Show";
-import Actor from "../models/Actor"
+import Actor from "../models/Actor";
 import HttpClient from "./http-client";
 
 class API extends HttpClient {
+  public constructor() {
+    super(process.env.REACT_APP_API_BASE_URL);
+  }
 
-
-    public constructor() {
-        super(process.env.REACT_APP_API_BASE_URL);
-    }
-
-    
-    public getLatestShows = () => this.instance.get<Schedule[]>('/schedule')
-    public getShow = (id: number | undefined) => this.instance.get<Show>(`/shows/${id}`)
-    public getShowCast = (id: number) => this.instance.get<Actor[]>(`/shows/${id}/cast`)
+  public getLatestShows = () => this.instance.get<Schedule[]>("/schedule");
+  public getShow = (id: number | undefined) =>
+    this.instance.get<Show>(`/shows/${id}`);
+  public getShowCast = (id: number) =>
+    this.instance.get<Actor[]>(`/shows/${id}/cast`);
 }
 
-export default API
+export default API;
